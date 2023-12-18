fb455293678d7c2409d9de163eb6ac4664b71fd0
yuzzin0121 <ydbwlsjki60@gmail.com>
2023-12-19 00:38:38 +0900

    create KakaoLogin Project

---
+++ 10주차/KakaoLoginSwiftUI/KakaoLoginSwiftUI/KakaoLoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoLoginSwiftUIApp.swift
+//  KakaoLoginSwiftUI
+//
+//  Created by 조유진 on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct KakaoLoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
